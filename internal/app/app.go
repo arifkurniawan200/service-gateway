0972a4628817092cd84a2ad44f8e87bdb53c8d08
arif kurniawan <arifkurniawan98@outlook.com>
2023-08-31 02:42:30 +0700

    init structure of service

---
+++ internal/app/app.go
@@ -0,0 +1,10 @@
+// Package app configures and runs application.
+package app
+
+import (
+	"github.com/service-gateway/config"
+)
+
+// Run creates objects via constructors.
+func Run(cfg *config.Config) {
+}
